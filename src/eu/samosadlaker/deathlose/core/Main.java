5d1719ba2f892b1313ad87ee018eef26dc9c890e
SamoSadlaker <samkosadlak@gmail.com>
2020-12-25 19:08:08 +0100

    Implements Vault & First setup for listener

--- src/eu/samosadlaker/deathlose/core/Main.java
+++ src/eu/samosadlaker/deathlose/core/Main.java
@@ -1,8 +1,10 @@
 package eu.samosadlaker.deathlose.core;
 
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -12,6 +14,8 @@ public class Main extends JavaPlugin {
     static Main plugin;
     CommandSender logger = Bukkit.getConsoleSender();
     PluginDescriptionFile pdf = this.getDescription();
+
+    public Economy eco;
 
     public static Main getPlugin() {
         return plugin;
@@ -27,6 +31,15 @@ public class Main extends JavaPlugin {
             saveDefaultConfig();
         }
         reloadConfig();
+
+        if(!setupEconomy()){
+            logger.sendMessage(Colors.formatColor("&b-------------------------------------"));
+            logger.sendMessage(Colors.formatColor("&4Disabling plugin &c" + pdf.getName() + " &c. Disabled due to no Vault dependency found!"));
+            logger.sendMessage(Colors.formatColor("&c" + pdf.getVersion() + " &f| &c" + pdf.getAuthors().toString() + " &f| &c" + pdf.getWebsite() ));
+            logger.sendMessage(Colors.formatColor("&b-------------------------------------"));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
 
         logger.sendMessage(Colors.formatColor("&b-------------------------------------"));
@@ -44,6 +57,18 @@ public class Main extends JavaPlugin {
         logger.sendMessage(Colors.formatColor("&c" + pdf.getVersion() + " &f| &c" + pdf.getAuthors().toString() + " &f| &c" + pdf.getWebsite() ));
         logger.sendMessage(Colors.formatColor("&b-------------------------------------"));
 
+    }
+
+    private boolean setupEconomy(){
+        if(getServer().getPluginManager().getPlugin("Vault") == null){
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        eco = rsp.getProvider();
+        return eco != null;
     }
 
 
--- src/eu/samosadlaker/deathlose/listeners/DeathListener.java
+++ src/eu/samosadlaker/deathlose/listeners/DeathListener.java
@@ -1,4 +1,17 @@
 package eu.samosadlaker.deathlose.listeners;
 
-public class DeathListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.net.http.WebSocket;
+
+public class DeathListener implements WebSocket.Listener {
+
+    @EventHandler
+    public void onPlayerDeath (PlayerDeathEvent e){
+
+
+
+    }
 }
